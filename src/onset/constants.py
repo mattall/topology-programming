4f2a38964f4627ea6c9a05959e9a975144e29a07
mattall <mhhall92@gmail.com>
2023-06-13 12:34:18 -0700

    fix relative dir

--- src/onset/constants.py
+++ src/onset/constants.py
@@ -7,8 +7,8 @@ PLOT_DIR = "data/plots/"
 
 IPV4 = ipaddress.IPv4Address._ALL_ONES
 IPV4LENGTH = 32
-SCRIPT_HOME = os.path.join(os.path.expanduser("~"), "topology_programming")
-USER_HOME = os.path.join(os.path.expanduser("~"))
+SCRIPT_HOME = os.path.abspath(".")
+USER_HOME = os.path.join(os.path.expanduser("~")) # For Yates executable
 ZERO_INDEXED = False
 PLT_BASE = 3
 PLT_HIGHT = 1.854
