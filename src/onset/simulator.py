a6656f2175cc5c1d39a1e282b5e9d3cb1f3c036c
matt <mhhall92@gmail.com>
2023-08-09 12:00:17 -0700

    formatting

--- src/onset/simulator.py
+++ src/onset/simulator.py
@@ -6,6 +6,7 @@ from onset.constants import SCRIPT_HOME
 from onset.defender import Defender
 from onset.utilities.config import CROSSFIRE
 from onset.optimization_two import Link_optimization
+
 # from mcf_net_difference import Link_optimization
 from onset.constants import USER_HOME
 from onset.utilities.diff_compare import diff_compare
@@ -63,51 +64,51 @@ class Simulation:
         Args:
             network_name (str):
             num_hosts (int): number of hosts in the network
-            
+
             test_name (str): Unique string, identifier for the simulation results
-            
+
             iterations (int, optional): Number of simulated epochs. Defaults to 0.
-            
+
             te_method (str, optional): Traffic engineering method - Must be supported
                 by Yates or self-implemented. Defaults to '-ecmp'.
-            
+
             start_clean (bool, optional): Overwrites simulation traffic matrices with new data. Defaults to False.
-            
+
             magnitude (int, optional): Mean value for traffic demand between nodes
                 - used only when start_clean is True. Defaults to 100*10**10.
-            
+
             traffic_file (str, optional): Path to traffic matrix file. Ignored when start_clean is True. Defaults to "".
-            
+
             topology_programming_method (str, optional): Topology programming method to use
                 Options: "cli", "cache", "onset", or "greylambda". Defaults to "".
-            
+
             fallow_transponders (int, optional): total fallow transponders per node. Defaults to 5.
-            
+
             use_heuristic (str, optional): Used when `topology_programming_method` is cache.
                 Limits the number of cached solutions to generate. Defaults to "".
-            
+
             candidate_link_choice_method (str, optional): "all" or "heuristic". Determines how to choose candidate links
                 when `topology_programming_method` is set to "cache". Defaults to "all".
-            
+
             congestion_threshold_upper_bound (float, optional): Congestion at or above this level triggers a topology
                 programming response from the network. Defaults to 0.8.
-            
+
             congestion_threshold_lower_bound (float, optional): Congestion at or below this level reverse any topology
                 programming update to the network. Defaults to 0.3.
-            
+
             attack_proportion (str, optional): Percent of total traffic that is from attackers.
                 Only used for file naming. Defaults to "".
-            
+
             shakeroute (bool, optional): shakeroute experiments simulate fiber cut failure scenarios. Defaults to False.
-            
+
             net_dir (bool, optional): director for network topology files. Defaults to False.
-            
+
             fallow_tx_allocation_strategy (str, optional): Describes how to allocate transponders. Defaults to 'static'.
                 can also be "dynamic" or "file". If it is "dynamic" then the top 90th percentile nodes get 'fallow_transponders' and others get 'fallow_transponders/2'.
-            
+
             fallow_tx_allocation_file (str, optional): _description_. Defaults to "".
                 Used when fallow_tx_allocation_strategy = "file", contains a path to a file that explicitly states the number of fallow transponders per node.
-            
+
             salt (str, optional): Used to generate unique file name for temp files when experiments with similar parameters are running simultaneously. Defaults to "".
         """
         makedirs(".temp", exist_ok=True)
@@ -265,7 +266,9 @@ class Simulation:
             "-out",
             result_path,
             "-budget",
-            "3", ">>", ".temp/yates.out"
+            "3",
+            ">>",
+            ".temp/yates.out",
         ]
 
         self._system(" ".join(command_args))
@@ -446,8 +449,15 @@ class Simulation:
         write_gml(gml_view, name)
         del gml_view
 
-    def perform_sim(self, circuits=1, start_iter=0, end_iter=0, repeat=False, unit="Gbps",
-                    demand_factor=1):
+    def perform_sim(
+        self,
+        circuits=1,
+        start_iter=0,
+        end_iter=0,
+        repeat=False,
+        unit="Gbps",
+        demand_factor=1,
+    ):
         if end_iter == 0:
             end_iter = self.iterations
 
@@ -533,7 +543,10 @@ class Simulation:
                     # don't read a new line if we are on the repetition step
                 else:
                     tm_i_data_from_file = fob.readline()
-                    tm_i_data = [str(float(i)*demand_factor) for i in tm_i_data_from_file.split()]
+                    tm_i_data = [
+                        str(float(i) * demand_factor)
+                        for i in tm_i_data_from_file.split()
+                    ]
                     tm_i_data_to_temp_file = " ".join(tm_i_data)
 
                 if i < start_iter:
@@ -569,7 +582,7 @@ class Simulation:
                     if self.topology_programming_method == "cli":
                         client = self.wolf.cli()
 
-                    elif ( # Heuristic method from very early testing.
+                    elif (  # Heuristic method from very early testing.
                         self.topology_programming_method == "cache"
                         and sig_add_circuits
                         and not circuits_added
@@ -592,7 +605,7 @@ class Simulation:
                                 "Adding {} ({}, {}) circuits.".format(
                                     circuits
                                 ),
-                                *new_circuit
+                                *new_circuit,
                             )
                             for _ in range(circuits):
                                 u, v = new_circuit
@@ -605,8 +618,8 @@ class Simulation:
                         #     self.exit_early = True
 
                     # elif self.strategy == 'onset' and sig_add_circuits and not circuits_added:
-                    
-                    elif ( # Method from TDSC-23 - Link-flood DDoS Defense
+
+                    elif (  # Method from TDSC-23 - Link-flood DDoS Defense
                         self.topology_programming_method == "onset"
                         and sig_add_circuits
                     ):
@@ -662,7 +675,7 @@ class Simulation:
 
                         sig_add_circuits = False
 
-                    elif ( # Method from PDP+OTP HotNets-23
+                    elif (  # Method from PDP+OTP HotNets-23
                         self.topology_programming_method == "OTP"
                         and sig_add_circuits
                     ):
@@ -679,10 +692,13 @@ class Simulation:
                             if edge_congestion_d[k] > 0.80
                         ]
 
-                        def find_shortcut_link(congested_edges):                            
+                        def find_shortcut_link(congested_edges):
                             node_counter = Counter()
                             message = "Looking for a shortcut link among: "
-                            congested_edges = [e.strip("()").replace("s", "").split(",") for e in congested_edges]
+                            congested_edges = [
+                                e.strip("()").replace("s", "").split(",")
+                                for e in congested_edges
+                            ]
                             for e in congested_edges:
                                 u, v = e
                                 node_counter.update((u, v))
@@ -690,15 +706,22 @@ class Simulation:
                             logger.info(message)
                             midpoint = max(node_counter, key=node_counter.get)
                             terminals = []
-                            for c in congested_edges: 
+                            for c in congested_edges:
                                 this = c[:]
                                 if midpoint in this:
                                     this.remove(midpoint)
                                     terminals.append(this[0])
-                            shortcuts = [c for c in combinations(terminals, 2) if c[0] != c[1] and c not in self.wolf.logical_graph.edges()]
-                            logger.info(f"Found the following shortcut: {shortcuts}")
+                            shortcuts = [
+                                c
+                                for c in combinations(terminals, 2)
+                                if c[0] != c[1]
+                                and c not in self.wolf.logical_graph.edges()
+                            ]
+                            logger.info(
+                                f"Found the following shortcut: {shortcuts}"
+                            )
                             return shortcuts
-                        
+
                         shortcuts = find_shortcut_link(congested_edges)
                         for edge in shortcuts:
                             u, v = edge
@@ -706,11 +729,11 @@ class Simulation:
                             v = int(v)
                             for _ in range(circuits):
                                 self.wolf.add_circuit(u, v, 100)
-                                flux_circuits.append((u,v))
+                                flux_circuits.append((u, v))
                         # flux_circuits.extend(congested_edges)
                         sig_add_circuits = False
 
-                    elif ( # Method from TNSM-23
+                    elif (  # Method from TNSM-23
                         self.topology_programming_method == "greylambda"
                         and sig_add_circuits
                     ):
@@ -734,9 +757,9 @@ class Simulation:
                                 self.wolf.add_circuit(u, v)
                         flux_circuits.extend(congested_edges)
                         sig_add_circuits = False
-                    
-                    elif ( # Bandwidth Variable Transceivers - Emulate RADWAN
-                        self.topology_programming_method == "BVT" 
+
+                    elif (  # Bandwidth Variable Transceivers - Emulate RADWAN
+                        self.topology_programming_method == "BVT"
                         and sig_add_circuits
                     ):
                         edge_congestion_file = path.join(
@@ -751,7 +774,6 @@ class Simulation:
                             for k in edge_congestion_d
                             if edge_congestion_d[k] == 1
                         ]
-                        # Increase bandwidth on congested links 20%                        
 
                         # for edge in congested_edges:
                         #     u, v = edge.strip("()").replace("s", "").split(",")
@@ -760,9 +782,9 @@ class Simulation:
                         #     for _ in range(circuits):
                         #         self.wolf.add_circuit(u, v)
                         # flux_circuits.extend(congested_edges)
-                        sig_add_circuits = False                
+                        sig_add_circuits = False
 
-                    elif ( # Temporary Bandwidth Expansion - i.e., Spiffy 
+                    elif (  # Temporary Bandwidth Expansion - i.e., Spiffy
                         self.topology_programming_method == "TBE"
                         and sig_add_circuits
                     ):
@@ -778,7 +800,7 @@ class Simulation:
                             for k in edge_congestion_d
                             if edge_congestion_d[k] == 1
                         ]
-                        # Increase bandwidth on congested links 20%                        
+                        # Increase bandwidth on congested links 20%
 
                         # for edge in congested_edges:
                         #     u, v = edge.strip("()").replace("s", "").split(",")
@@ -787,7 +809,7 @@ class Simulation:
                         #     for _ in range(circuits):
                         #         self.wolf.add_circuit(u, v)
                         # flux_circuits.extend(congested_edges)
-                        sig_add_circuits = False                
+                        sig_add_circuits = False
 
                     # Net Recon Defense Method
                     elif self.topology_programming_method == "skinwalker":
@@ -796,7 +818,7 @@ class Simulation:
                             # BUDGET=0,
                             demand_matrix_file=self.nonce,
                             network=self.network_name,
-                            core_G=self.wolf.base_graph.copy(as_view=True)
+                            core_G=self.wolf.base_graph.copy(as_view=True),
                         )
                         # optimizer.run_model()
                         if self.te_method == "-ecmp":
@@ -813,16 +835,16 @@ class Simulation:
                         add_links = []
                         drop_links = []
                         # optimizer.LINK_CAPACITY *= max_load
-                        add_links, drop_links = optimizer.mcf()          
-                        # result_topo, add_links, drop_links = optimizer.optimize()                        
-                        # result_topo, add_links, drop_links = optimizer.run_model_max_diff_ignore_demand()                        
+                        add_links, drop_links = optimizer.mcf()
+                        # result_topo, add_links, drop_links = optimizer.optimize()
+                        # result_topo, add_links, drop_links = optimizer.run_model_max_diff_ignore_demand()
 
                         circuit_tag = ""
                         new_circuit = add_links[:]
                         if drop_links:
                             for drop_circuit in drop_links:
                                 u, v = drop_circuit
-                                self.wolf.drop_circuit(u,v)
+                                self.wolf.drop_circuit(u, v)
 
                         if add_links:
                             for nc in new_circuit:
@@ -866,7 +888,9 @@ class Simulation:
                         updated_topology_file + ".gml"
                     )
                     Gml_to_dot(
-                        self.wolf.logical_graph, iteration_topo + ".dot", unit=unit
+                        self.wolf.logical_graph,
+                        iteration_topo + ".dot",
+                        unit=unit,
                     )
 
                     # Draw the link graph for the instanced topology.
@@ -880,7 +904,7 @@ class Simulation:
                     if len(new_circuit) > 0:
                         if self.topology_programming_method == "skinwalker":
                             reconfig_time = 1
-                        else: 
+                        else:
                             reconfig_time = get_reconfig_time(
                                 updated_topology_file + ".gml", new_circuit
                             )
@@ -971,8 +995,8 @@ class Simulation:
                     return return_data
 
                 # finally:
-                    # # Remove the temp file.
-                    # self._system("rm %s" % temp_tm_i)
+                # # Remove the temp file.
+                # self._system("rm %s" % temp_tm_i)
 
                 PREV_ITER_ABS_PATH = ITERATION_ABS_PATH
                 # command_args = ["yates", iteration_topo, temp_tm_i, temp_tm_i, hosts,
@@ -1123,16 +1147,20 @@ class Simulation:
                 logger.debug("traffic file found.")
                 lines = count_lines(traffic_file)
                 if lines < self.iterations:
-                    logger.error("traffic file found, but has too few lines. expected: {} got: {}".format(
-                    self.iterations, lines
-                ))
+                    logger.error(
+                        "traffic file found, but has too few lines. expected: {} got: {}".format(
+                            self.iterations, lines
+                        )
+                    )
                     assert False
                 logger.debug("traffic file line-count passed.")
                 M = loadtxt(traffic_file)
                 if sqrt(len(M)) != self.num_hosts:
-                    logger.error("WRONG NUMBER OF ENTRIES IN MATRIX YOU FUCKING IDIOT")
+                    logger.error(
+                        "WRONG NUMBER OF ENTRIES IN MATRIX YOU FUCKING IDIOT"
+                    )
                     assert False
-                
+
             except AssertionError:
                 if self.start_clean:
                     pass
@@ -1140,7 +1168,7 @@ class Simulation:
                     logger.error(
                         "Error verifying traffic file Create one now? [y/n]"
                     )
-                    
+
                     create = input()
                     if create.lower().startswith("y"):
                         pass
